2f8222b8e0ed27fa33bb2e7b25d050ae5ecd55d6
karich97 <kcm281997@gmail.com>
2023-12-06 13:23:44 +0300

    commit from AlfaBank as Aston worker

--- HelloWorld/src/MainClass.java
+++ HelloWorld/src/MainClass.java
@@ -2,5 +2,6 @@ public class MainClass {
     public static void main(String[] args) {
         System.out.println("Hello, Git!");
         System.out.println("Welcome to Aston company");
+        System.out.println("I get new macbook 16 inch m1");
     }
 }
